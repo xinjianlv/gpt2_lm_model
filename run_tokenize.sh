239a7ca913ea2d0d5304538ec8489844bd44b3db
lvxinjian <lvxinjian@wanda.cn>
2019-10-07 12:24:41 +0800

    add

--- run_tokenize.sh
+++
@@ -1,1 +0,0 @@
-nohup python utils.py --input_data_file ../data/dpcq/dpcq.txt --tokenized_data_path ../data/dpcq/tokenized/   --vocab_file  ../data/vocab_small.txt --num_pieces 10 > log.tokenize.out &
