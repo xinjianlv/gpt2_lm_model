912e9ea9cac4e8edc5500362d32218ed92240deb
lvxinjian <lvxinjian@wanda.cn>
2019-10-10 17:13:43 +0800

    add

--- run_train.sh
+++
@@ -1,7 +0,0 @@
-nohup python3 src/language/train.py \
- --train_batch_size 4 \
- --dataset_path ./data/dqcp.txt  \
- --tokenized_data_path ./data/dpcq/tokenized/ \
- --model_config_file ./config/model_config_small.json \
- --vocab_file ./config/vocab_small.txt \
- > log.train.out &
