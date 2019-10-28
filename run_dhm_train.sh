705f6c455def77372015b7f67a81064e5157b888
lvxinjian <lvxinjian@wanda.cn>
2019-10-28 17:43:38 +0800

    add

--- run_dhm_train.sh
+++ run_dhm_train.sh
@@ -1,6 +1,8 @@
 nohup python3 ./src/dialogue/train.py \
- --train_batch_size 4 \
- --dataset_path ./data/text.data/multi_1_4.4_100w.data  \
+ --train_batch_size 1 \
+ --data_name small.txt \
+ --dataset_cache  ./cache/ \
+ --dataset_path ./data/xiaohuangji/small.txt \
  --model_config_file ./config/model_config_small.json \
- --vocab_file ./config/vocab_small.txt
+ --vocab_file ./config/vocab_small.txt \
  > log.train.out &
